a2c79b3fdb830dcc05b5ac18ecddf48921a0e5ab
Muhammad-217 <you@example.com>
2022-04-12 19:48:53 -0700

    added note taking area with save and cancel buttons

--- src/app.js
+++ src/app.js
@@ -5,3 +5,27 @@ const notes = [
     id: 1 
   }
 ]
+
+const newNoteButton = document.querySelector('.fa-circle-plus') 
+const createNote = document.querySelector('.create-note-area')
+const writeNote = document.querySelector('.write-note-area')
+const readNote = document.querySelector('.read-note-area') 
+const noteArea = `<textarea id='add-new-note' rows="30" cols="60"placeholder = "1.) Title \n2.) Body-Text"></textarea>`
+const NotesList = document.querySelector('.notes-list')
+
+const cancelNoteButton = document.createElement('button')
+cancelNoteButton.innerHTML = 'Cancel'
+
+const saveNoteButton = document.createElement('button')
+saveNoteButton.innerHTML = 'Save'
+
+const closeNoteButton = document.createElement('button')
+closeNoteButton.innerHTML = 'Close'
+
+function NewNoteBox() {
+writeNote.insertAdjacentHTML('afterbegin', noteArea)
+writeNote.appendChild(saveNoteButton)
+writeNote.appendChild(cancelNoteButton)
+newNoteButton.remove()
+}
+newNoteButton.addEventListener('click', NewNoteBox)
